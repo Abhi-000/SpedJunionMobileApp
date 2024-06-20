c1966d5048cd63fc57fc4d9ca68fa53991b0a932
abhishekShah-000 <abhishekshah@toteminteractive.in>
2024-06-21 01:10:42 +0530

    Bug fix get books

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE_URL = "https://testing.spedathome.com:7253/api";
+const API_BASE_URL = "https://testing.spedathome.com:7233/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
